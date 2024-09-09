4a25ec8bd456da9741f910bf4b801ed091588a7a
Nick <nick.sherbina@gmail.com>
2024-09-09 11:56:00 +0200

    database setup. Profile, arts tables, Oauth

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Create .env file:
+
+DB_USER=your_user </br>
+DB_HOST=localhost </br>
+DB_DATABASE=generativeArtPlatform</br>
+DB_PASSWORD=your_password</br>
+DB_PORT=your_port</br>
+OAUTH_CLIENT_ID=your_client_id</br>
+OAUTH_CLIENT_SECRET=your_client_secret</br>
+
+Create database called generativeArtPlatform</br>
+Run the following command to create the tables:</br>
+npx ts-node src/setupdatabase.ts</br>
+Run the following command to start the server:</br>
+npx ts-node src/main.ts</br>
---
+++ src/setupdatabase.ts
@@ -0,0 +1,63 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Create a new Pool instance
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+});
+
+// Function to execute a query
+async function query(text: string, params?: any[]): Promise<void> {
+    try {
+        await pool.query(text, params);
+        console.log('Query executed successfully');
+    } catch (err) {
+        console.error('Query error', err);
+    }
+}
+
+// Function to create tables
+async function createTables() {
+    try {
+
+        // Create profiles table
+        await query(`
+            CREATE TABLE IF NOT EXISTS profiles (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                oauth_provider TEXT NOT NULL,
+                oauth_id TEXT NOT NULL UNIQUE
+            );
+        `);
+
+        console.log('Profiles table created or already exists');
+
+        // Create arts table
+        await query(`
+            CREATE TABLE arts (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                file_url TEXT NOT NULL,
+                preview_images_url TEXT[],
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        console.log('Arts table created or already exists');
+
+    } catch (err) {
+        console.error('Error creating tables', err);
+    } finally {
+        // Close the database pool
+        await pool.end();
+    }
+}
+
+// Run the createTables function
+createTables();
